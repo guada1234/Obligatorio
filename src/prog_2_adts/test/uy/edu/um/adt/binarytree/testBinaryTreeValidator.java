918a6edce977510cb3e9884d93dd00ce84770d03
guadaluperial <guadaluperial1234@gmail.com>
2024-05-24 18:37:38 -0300

    agrego tests hash searchT, findposition

--- src/prog_2_adts/test/uy/edu/um/adt/binarytree/testBinaryTreeValidator.java
+++ src/prog_2_adts/test/uy/edu/um/adt/binarytree/testBinaryTreeValidator.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class testBinaryTreeValidator {
 
-    private BinaryTree<Integer,String> binaryTree;
+
 
     @Test
     public void testSearchAdd() throws InvalidKey, EmptyTree {
