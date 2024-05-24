e66c7a99fea0e520325dbe979ebbc8683d395753
guadaluperial <guadaluperial1234@gmail.com>
2024-05-24 11:18:49 -0300

    hash final

---
+++ src/prog_2_adts/src/uy/edu/um/adt/linkedlist/MyList.java
@@ -0,0 +1,18 @@
+package prog_2_adts.src.uy.edu.um.adt.linkedlist;
+
+public interface MyList<T> {
+
+    void add(T value);
+
+    T get(int position);
+
+    boolean contains(T value);
+
+    void remove(T value);
+
+    int size();
+
+    Node<T> getFirst();
+
+    boolean isEmpty();
+}
---
+++ src/prog_2_adts/src/uy/edu/um/adt/linkedlist/Node.java
@@ -0,0 +1,54 @@
+package prog_2_adts.src.uy.edu.um.adt.linkedlist;
+
+import java.util.Objects;
+
+public class Node<T> {
+
+    private T value;
+
+    private Node<T> next;
+
+    private int priority;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(value, node.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
