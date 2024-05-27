f0498f2270a6bbd5802e4ca4fb12810342b69dd8
camilapereira <cpereira3@correo.um.edu.uy>
2024-05-27 12:45:16 -0300

    getter y setter y constructor de Cancion

--- src/prog_2_adts/classes/Cancion.java
+++ src/prog_2_adts/classes/Cancion.java
@@ -7,4 +7,52 @@ public class Cancion {
     private int posicion;
     private Date fecha;
     private int tempo;
+
+    public Cancion(String titulo, String artista, int posicion, Date fecha, int tempo) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.posicion = posicion;
+        this.fecha = fecha;
+        this.tempo = tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
 }
