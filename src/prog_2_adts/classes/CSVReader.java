2e1c1f5a8f9acddb8618f2b05d5384522df396cf
guadaluperial <guadaluperial1234@gmail.com>
2024-05-28 09:43:52 -0300

    class CSVReader, imprime bien todos los datos por terminal

--- src/prog_2_adts/classes/CSVReader.java
+++ src/prog_2_adts/classes/CSVReader.java
@@ -7,18 +7,16 @@ import java.io.IOException;
 public class CSVReader {
 
     public static void main(String[] args) {
-        String csvFile = "/Users/guadaluperial/Desktop/Spotify_canciones.csv";  // replace with your CSV file path
+        String csvFile = "/Users/guadaluperial/Desktop/Spotify_canciones.csv";
         String line;
         String csvSplitBy = ",";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             int i=0;
             while ((line = br.readLine()) != null) {
-                // Use comma as separator
+
                 String[] fields = line.split(csvSplitBy);
                 i++;
-
-                // Print each field (for demonstration purposes)
                 for (String field : fields) {
                     System.out.print(i+". "+field);
                 }
