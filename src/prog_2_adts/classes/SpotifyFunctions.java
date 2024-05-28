e748965882067db30982be456507086479531b3c
guadaluperial <guadaluperial1234@gmail.com>
2024-05-28 10:19:44 -0300

    cambio mostrarTop10

--- src/prog_2_adts/classes/SpotifyFunctions.java
+++ src/prog_2_adts/classes/SpotifyFunctions.java
@@ -1,5 +1,9 @@
 package prog_2_adts.classes;
 
+import prog_2_adts.src.uy.edu.um.adt.linkedlist.MyLinkedListImpl;
+import prog_2_adts.src.uy.edu.um.adt.linkedlist.MyList;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,13 +21,16 @@ public class SpotifyFunctions {
         String pais = scanner.nextLine();
         System.out.println("Ingrese la fecha (YYYY-MM-DD):");
         String fecha = scanner.nextLine();
-
-        List<Cancion> top10 = SpotifyFunctions.obtenerTop10(pais, fecha);
+        MyList<Cancion> cancionesTop10 = new MyLinkedListImpl();
+        cancionesTop10 = obtenerTop10(pais, fecha);
 
         System.out.println("Top 10 canciones en " + pais + " el " + fecha + ":");
         for (Cancion cancion : top10) {
             System.out.println(cancion.getPosicion() + ". " + cancion.getTitulo() + " - " + cancion.getArtista());
         }
+    }
+    public MyList obtenerTop10(String pais, Date fecha){
+
     }
 }
 
