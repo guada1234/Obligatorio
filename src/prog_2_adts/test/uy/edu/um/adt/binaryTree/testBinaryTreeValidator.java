86d96b88431f8d1507d96410f88d374733e35169
camilapereira <cpereira3@correo.um.edu.uy>
2024-06-19 19:26:31 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ src/prog_2_adts/test/uy/edu/um/adt/binaryTree/testBinaryTreeValidator.java
@@ -0,0 +1,14 @@
+package prog_2_adts.test.uy.edu.um.adt.binaryTree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import prog_2_adts.src.uy.edu.um.adt.BinaryTree2.MyBinarySearchTreeImpl;
+import prog_2_adts.src.uy.edu.um.adt.linkedlist.MyLinkedListImpl;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class testBinaryTreeValidator {
+
+}
--- src/prog_2_adts/test/uy/edu/um/adt/hash/testHashValidator.java
+++ src/prog_2_adts/test/uy/edu/um/adt/hash/testHashValidator.java
@@ -1,6 +1,60 @@
 package prog_2_adts.test.uy.edu.um.adt.hash;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import prog_2_adts.src.uy.edu.um.adt.hash.HashImpl;
+import prog_2_adts.src.uy.edu.um.adt.hash.InformacionInvalida;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class testHashValidator {
+    private HashImpl<Integer, String> hash;
+
+    @BeforeEach
+    void setUp() {
+        hash = new HashImpl<>(10);
+    }
+
+    @Test
+    void testAddAndContainsKey() {
+        hash.add(5, "cinco");
+        assertTrue(hash.containsKey(5));
+        assertFalse(hash.containsKey(4));
+    }
+
+    @Test
+    void testDelete() {
+        hash.add(5, "cinco");
+        try {
+            hash.delete(5);
+        } catch (InformacionInvalida e) {
+            fail("Unexpected exception thrown: " + e.getMessage());
+        }
+        assertFalse(hash.containsKey(5));
+    }
+
+    @Test
+    void testGet() {
+        hash.add(5, "cinco");
+        assertEquals("five", hash.get(5));
+        assertNull(hash.get(4));
+    }
+
+    @Test
+    void testDeleteException() {
+        assertThrows(InformacionInvalida.class, () -> hash.delete(11));
+    }
+
+    @Test
+    void testGetDataException() {
+        assertThrows(InformacionInvalida.class, () -> hash.getData(11));
+    }
+
+    @Test
+    void testGetNodeException() {
+        assertThrows(InformacionInvalida.class, () -> hash.getNode(11));
+    }
 
 
 }
